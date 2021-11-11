82de0dac84c52b0f76a6c06c3736fa5c0c76cdb7
Peru Erro <peru.erro@opendeusto.es>
2021-11-11 17:12:49 +0100

    Creción del metodo seleccionarUsuario

--- src/db/SelectData.java
+++ src/db/SelectData.java
@@ -5,18 +5,71 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import clases.Usuario;
 
 
 public class SelectData {
-	private Connection dbConnect;
-	public void SelectData() throws SQLException{
-		this.dbConnect=DriverManager.getConnection("proyecto.db");
-	
-		
-	}
-	public void selectData() throws SQLException {
-		
-		Statement stmt =dbConnect.createStatement();
-		ResultSet rs=stmt.executeQuery("SELECT ALUMNO WHERE NOMBRE = 'Jon'");
-	}
+	/**
+     * Conectar a la bd bdproyecto.db 
+     * @return la conexion
+     */
+    private static Connection connect()
+    {
+        // SQLite connection string
+        String name = "bdproyecto.db";
+        String url = "jdbc:sqlite:" + name;
+        Connection conn = null;
+
+        try
+        {
+            conn = DriverManager.getConnection(url);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    /**
+     * Seleciona todo lo de la tabla Usuario
+     */
+    public static ArrayList<Usuario> seleccionarUsuario()
+    {
+        String sql = "SELECT nombre, nombreUsuario, contrasenya, admin FROM Usuario";
+        ArrayList<Usuario> usuarios=new ArrayList<Usuario>();
+        try
+                (
+                        Connection conn = connect();
+                        Statement stmt  = conn.createStatement();
+                       
+                )
+        {
+        	ResultSet rs=stmt.executeQuery(sql);
+            
+            while (rs.next())
+            {
+                System.out.println
+                        (
+                                rs.getString("nombre") +  "\t" +
+                                rs.getString("nombreUsuario") + "\t" +
+                                rs.getString("contrasenya") + "\t" +
+                                rs.getInt("admin")
+                        );
+            }
+            
+        
+        } catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+		return usuarios;
+    }
+    public static void main(String[]args){
+    	ArrayList<Usuario> u= SelectData.seleccionarUsuario();
+    	System.out.println(u);
+    }
+    
+
 }
