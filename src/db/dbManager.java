10b66d0024c53356deebaddfa0ad472206812db6
Peru Erro <peru.erro@opendeusto.es>
2021-11-12 00:54:50 +0100

    Actualizacion adManager

--- src/db/dbManager.java
+++ src/db/dbManager.java
@@ -3,24 +3,33 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+/**
+ * Creacion de tablas
+ * @author Peru Erro Arrue
+ * @version 1.0
+ */
 public class dbManager {
 	
 	    /**
-	     * Create a new table in the test database
+	     * Crear una tabla en la base de datos
 	     *
 	     */
-	    public static void createNewTable()
+	    public static void crearTablaJugador()
 	    {
-	        // SQLite connection string
-	        String name = "HelloWorld.db";
+	        
+	        String name = "bdproyecto.db";
 	        String url = "jdbc:sqlite:" + name;
 
-	        // SQL statement for creating a new table
-	        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
-	                + "    id integer PRIMARY KEY,\n"
-	                + "    name text NOT NULL,\n"
-	                + "    capacity real\n"
+	        
+	        String sql = "CREATE TABLE IF NOT EXISTS Jugador (\n"
+	                + "    nombre text not null,\n"
+	                + "    edad interger not null,\n"
+	                + "    nacionalidad text not null,\n"
+	                + "    titulos interger not null,\n"
+	                + "    pala text not null,\n"
+	                + "    manoHabil text not null,\n"
+	                + "    posicion text not null\n"
+	          
 	                + ");";
 
 	        try
@@ -33,16 +42,17 @@ public class dbManager {
 	            stmt.execute(sql);
 	        } catch (SQLException e)
 	        {
-	            System.out.println(e.getMessage());
+	        	e.printStackTrace();
 	        }
 	    }
 
 	    /**
+	     * Metodo principal para poder utilizar el metodo crearTablaJugador
 	     * @param args the command line arguments
 	     */
 	    public static void main(String[] args)
 	    {
-	        createNewTable();
+	        crearTablaJugador();
 	    }
 
 	}
