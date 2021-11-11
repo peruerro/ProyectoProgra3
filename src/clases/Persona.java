34c9603a4c7cb2347dcf9fab49cf7440e225803f
Peru Erro <peru.erro@opendeusto.es>
2021-11-12 00:49:57 +0100

    actualización clase Persona

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,12 +1,34 @@
 package clases;
-
+/**
+ * Representa a las personas en general y despues las clases Jugador y Entrenador la heredan
+ * @author Peru Erro Arrue
+ * @version 1.0
+ *
+ */
 public class Persona {
-
+	/**
+	 * Indica el nombre de la persona
+	 */
 	private String nombre;
+	/**
+	 * Indica la edad de la persona
+	 */
 	private int edad;
+	/**
+	 * Indica donde nacio la persona
+	 */
 	private String nacionalidad;
+	/**
+	 * Indica los titulos ganados por la persona
+	 */
 	private String titulos;
-	
+	/**
+	 * Rellena los parametros de la clase Persona
+	 * @param nombre El nombre de la persona
+	 * @param edad La edad de la persona
+	 * @param nacionalidad Donde nacio la persona
+	 * @param titulos que titulos ha ganado la persona
+	 */
 	public Persona(String nombre, int edad, String nacionalidad, String titulos) {
 		super();
 		this.nombre = nombre;
@@ -14,27 +36,59 @@ public class Persona {
 		this.nacionalidad = nacionalidad;
 		this.titulos=titulos;
 	}
+	/**
+	 * Devuelve el nombre de la Persona
+	 * @return Un String con el nombre de la Persona
+	 */
 	public String getNombre() {
 		return nombre;
 	}
+	/**
+	 * Modifica el nombre de la Persona
+	 * @param nombre El nuevo nombre de la persona
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * Devuelve la edad de la Persona
+	 * @return Un int con la edad de la persona
+	 */
 	public int getEdad() {
 		return edad;
 	}
+	/**
+	 * Modifica la edad de la persona
+	 * @param edad la nueva edad de la persona
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+	/**
+	 * Devuelve la nacionalidad de la persona
+	 * @return un String con la nacionalidad
+	 */
 	public String getNacionalidad() {
 		return nacionalidad;
 	}
+	/**
+	 * Modifica la nacionalidad
+	 * @param nacionalidad la nueva nacionalidad de la persona
+	 */
 	public void setNacionalidad(String nacionalidad) {
 		this.nacionalidad = nacionalidad;
 	}
+	/**
+	 * Devuelve los titulos de la persona
+	 * @return un String con los titulos
+	 */
 	public String getTitulos() {
 		return titulos;
 	}
+	/**
+	 * Modifica los titulos de la persona
+	 * @param titulos los nuevos titulos de la persona
+	 */
 	public void setTitulos(String titulos) {
 		this.titulos = titulos;
 	}
